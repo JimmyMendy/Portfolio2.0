29f26d61c145adc8ac0e8e7be3474953c3e5d8c0
jimmymendy <jimmymendy@Pros-MacBook-Pro.local>
2023-03-19 18:22:56 +0000

    some changes

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-transparent z-10 h-24 sticky pt-8 md:pt-10">
+    <div className='bg-transparent z-10 h-24 sticky pt-8 md:pt-10'>
       <h1>Hello I am the Header</h1>
+      <p>test test test </p>
     </div>
   );
 }
